a806611a9e2e476c379833f896fd10e3857ba91b
shah253kt <shah25392@gmail.com>
2016-08-17 13:46:21 +0800

    Update script.js
    Added some commentary.

--- script.js
+++ script.js
@@ -27,10 +27,12 @@ var Script = function() {
 			lockControl = true;
 			console.log("Turning to page " + currentPage);
 
+			//Exit animation
 			$("#page" + lastPage).animate({
 				top: "-100%"
 			}, 400, finishedExit);
 
+			//Entry animation
 			$("#page" + currentPage).animate({
 				top: "0"
 			}, 400, finishedEntry);
@@ -43,6 +45,7 @@ var Script = function() {
 
 	function finishedExit() {
 		// console.log("finishedExit");
+		//Reset the position of the <div> element to the starting position.
 		document.getElementById("page" + lastPage).style.top = "100%";
 		lastPage = currentPage;
 		lockControl = false;
